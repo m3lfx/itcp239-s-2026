763ef74037cad776e684e8b8266e79e7622931f6
rommel dalisay <rommelmdalisay@gmail.com>
2025-12-16 14:56:42 +0800

    products

---
+++ Screens/Product/ProductContainer.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet, ActivityIndicator } from 'react-native'
+import ProductList from './ProductList'
+const data = require('../../assets/data/products.json')
+
+const ProductContainer = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        setProducts(data);
+
+        return () => {
+            setProducts([])
+        }
+    }, [])
+
+    return (
+        <View>
+            <Text>Product Container</Text>
+            <View style={{ marginTop: 200 }} >
+                <FlatList
+
+                    columnWrapperStyle={{ justifyContent: 'space-between' }}
+                    numColumns={2}
+                    data={products}
+
+                    renderItem={({ item }) => <ProductList key={item.id} item={item} />}
+                    keyExtractor={item => item.name}
+                />
+            </View>
+        </View>
+    )
+}
+
+export default ProductContainer;
